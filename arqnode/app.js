2a0b12adc6d9c5ef059788ad558420012f3c0cc1
Romulo Antonio Azambuja <tec.romulo.azambuja@gmail.com>
2021-02-15 11:04:59 -0300

    comentarios

--- arqnode/app.js
+++ arqnode/app.js
@@ -5,8 +5,13 @@ const routher = express.Router();
 const port = 3000;
 
 app.get('/',(req,res)=> {
+    // utilizo o __dirname para indicar o diretorio atual 
     res.sendFile(__dirname + '/recurso/index.html')
 });
 app.listen(port,() => {
     console.log('teste ouvindo porta')
 });
+// para ouvir a porta 3000 no prompt deve iniciar o arquivo app.js comando node app.js na pasta onde esta o arquivo
+
+/*ACABEI MOVENDO TODOS OS ARQUIVOS HTML PARA UMA PASTA DENTRO DE ONDE ESTÃO OS ARQUIVOS NODE 
+NÃO SEI SE ISSO É CORRETO*/
